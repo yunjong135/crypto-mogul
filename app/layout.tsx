ba1223f053bf8481d4b8001ee9d839da06b79afb
yunjong135 <yunjong.park@datadoghq.com>
2025-09-13 13:44:06 +0900

    Fix hydration error in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,16 +16,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <head>
-        <style>{`
-html {
-  font-family: ${GeistSans.style.fontFamily};
-  --font-sans: ${GeistSans.variable};
-  --font-mono: ${GeistMono.variable};
-}
-        `}</style>
-      </head>
+    <html lang="en" className={`${GeistSans.className} ${GeistMono.variable}`}>
       <body>
         {children}
         <Analytics />
